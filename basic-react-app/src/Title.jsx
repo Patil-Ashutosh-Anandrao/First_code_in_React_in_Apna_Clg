31c1c3c9f4d450dd97743fe0b2a937c578b131d7
Ashu Patil <ashutoshpa4367@gmail.com>
2024-05-09 18:01:02 +0530

    feat : done

--- basic-react-app/src/Title.jsx
+++ basic-react-app/src/Title.jsx
@@ -1,5 +1,17 @@
 function Title() {
-  return <h1>This is Title !</h1>
+
+  let name = "Ashu";
+
+  return (
+  <>
+    <h1>This is Title !</h1>
+
+    <h2> JSX with curly Brases </h2>
+     2 * 2 = {2*2}  {/* code inside { ... } this means it is treated as JS logic and perform operations !!!   */}
+    <p>Hi, {name} </p>
+    <p>Hi, {name.toUpperCase()} </p>
+  </>
+  );
 }
 
 export default Title; 
