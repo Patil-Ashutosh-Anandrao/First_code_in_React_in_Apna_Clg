50ddc699e3d340542727e91e1bb7a1d9f37539bd
Ashu Patil <ashutoshpa4367@gmail.com>
2024-05-10 11:24:26 +0530

    feat : update

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,13 +1,20 @@
 import "./Product.css";
 
-function Product( {title, price, features, features2 } ){
+function Product( {title, price, features6 } ){
+    //const list = features6.map( (feature) => <li>{feature}</li>);
     return(
         <div className="Product">
             <h3>Product {title}</h3>
             <h5>Price = {price} </h5>
+            {/* <p>Features = {list} </p> */}
+
+            {/* we can also write in this manner  */}
+            <p>{ features6.map( (feature) => <li>{feature}</li>)} </p>
+            
+            {/*             
             <p>Features = {features} </p>
-            <p>Features2 = {features2.a} </p>
-            {/* <p>Features2 = {features2.b} </p>
+            <p>Features2 = {features2.a} </p> 
+            <p>Features2 = {features2.b} </p>
             <p>Features2 = {features2.c} </p> */}
         </div>
     );
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -8,18 +8,30 @@ function ProductTab(){
     // store value in object 
     let options2 = {a: "4GB RAM", b: "64GB Storage", c: "13MP Camera"};
 
+    //rendering array elements using array element 
+    let options3 = [ <li>"4GB RAM"</li>, 
+                     <li> "64GB Storage"</li>, 
+                     <li> "13MP Camera"</li> 
+                   ];
+
+    //rendering array elements using array values using MAP function
+    let options4 = ["4GB RAM", "64GB Storage", "13MP Camera"];
+
     return(
         <>
             <Product title="Mobile" 
                      price={10000} 
-                     features={options} 
-                     features2={options2}
-                     
-                     features3={["4GB RAM", "64GB Storage", "13MP Camera"]}  //we can also write array like this
-                     features4={{a: "4GB RAM", b: "64GB Storage", c: "13MP Camera"}} //we can also write object like this
+                     features6={options4}
+
+
+                    //  features={options} 
+                    //  features2={options2}
+                    //  features3={["4GB RAM", "64GB Storage", "13MP Camera"]}  //we can also write array like this
+                    //  features4={{a: "4GB RAM", b: "64GB Storage", c: "13MP Camera"}} //we can also write object like this
+                    //  features5={options3}
             />
-            <Product title="Tablet" price={40000}/>
-            <Product title="Laptop" price={50000}/>
+            {/* <Product title="Tablet" price={40000}/>
+            <Product title="Laptop" price={50000}/> */}
         </>
     );
 }
